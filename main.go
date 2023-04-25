7371c96e33ba568645163b5cf774d6b947b3e0b9
ali <kasymkhan_05_05@bk.ru>
2023-04-25 18:26:53 +0600

    gin, api

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", pong)
+	r.GET("/user", getUser)
+	r.POST("/user", postUser)
+	r.Run(":8080")
+}
+
+func pong(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+func getUser(c *gin.Context) {
+	id := c.Query("id")
+	user := struct {
+		Id string `json:"id"`
+		Name string `json:"name"`
+	}{id, "Ali"}
+	c.JSON(200,gin.H{
+		"user": user,
+	})
+}
+
+func postUser(c *gin.Context) {
+	user := struct {
+		Id string `json:"id"`
+		Name string `json:"name"`
+	}{}
+	c.BindJSON(&user)
+	user.Name = "Human"
+	c.JSON(200, gin.H{
+		"user": user,
+	})
+}
